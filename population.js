a26220d53514428adbb8d87319d47fbba142380e
Renzo Silva <renzoasilva@gmail.com>
2022-07-22 22:49:12 -0400

    more changes

--- population.js
+++ population.js
@@ -1,10 +1,10 @@
-Challenge 1 : Scientists have discovered that in four decades, the world will EXPLODE! It will also take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
+/*Challenge 1 : Scientists have discovered that in four decades, the world will EXPLODE! It will also take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
 You must calculate the number of people there will be in three decades from now.
 The variable population is the world population now.
 Assume that every month, someone gives birth to more people n.
 Return the number of people there will be when the spaceship is complete.
 Examples
-futurePeople(256, 2) ➞ 976
+futurePeople(256, 2) ➞ 976 */
 
 futurePeople(3248, 6) ➞ 5408
 
